ef632f50ceac6030d58c8c003480635838715f8f
Dostonbek Oripjonov <dostonbek.oripjonov@mail.ru>
2023-05-20 08:41:22 +0500

    Building game cards

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,14 @@
 export interface Game {
     id: number
     name: string
+    background_image?: string
 }
 
 export interface FetchGameResposne {
     count: number
     results: Game[]
+}
+
+export interface GameCardProps {
+    game: Game
 }
