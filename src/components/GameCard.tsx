0b1f0b1cb2f4e69381a1e259e6347c5df1d13e1c
Dastnbek <dostonbek.oripjonov@mail.ru>
2023-05-22 08:52:20 +0500

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,12 +2,15 @@ import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react"
 import { GameCardProps } from "../types"
 import { PlatformIconList } from "./PlatformIconList"
 import { CriticScore } from "./CriticScore"
+import { getCroppedImageUrl } from "../services/image-url"
 
 export const GameCard = ({ game }: GameCardProps) => {
     const { name, parent_platforms, metacritic, background_image } = game
+    const image_url = getCroppedImageUrl(background_image)
+
     return (
         <Card borderRadius={10} overflow="hidden">
-            <Image src={background_image} />
+            <Image src={image_url} />
             <CardBody>
                 <Heading fontSize="2xl">{name}</Heading>
                 <HStack justifyContent="space-between">
